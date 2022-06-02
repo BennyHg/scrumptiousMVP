e69c0e0d4a2fad8c945187a471e47b550a38613a
benny.huang <bhuang997@gmail.com>
2022-06-02 11:48:11 -0700

    Scrumptious MVP

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-
+from recipes.models import Recipe, Step
 
 # Register your models here.
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Step, StepAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -2,3 +2,19 @@ from django.db import models
 
 
 # Create your models here.
+class Recipe(models.Model):
+    name = models.CharField(max_length=125)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name + " by " + self.author
+
+class Step(models.Model):
+    order = models.SmallIntegerField()
+    directions = models.TextField()
+    author = models.ForeignKey("Recipe", related_name="steps", on_delete=models.CASCADE)
