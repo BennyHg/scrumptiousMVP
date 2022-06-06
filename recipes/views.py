a197e6555500f5d7981c00e169efccf60e52f7f0
benny.huang <bhuang997@gmail.com>
2022-06-06 12:16:50 -0700

    Converted the create view for a Recipe

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 
 from recipes.forms import RatingForm
@@ -10,22 +12,6 @@ try:
 except Exception:
     RecipeForm = None
     Recipe = None
-
-
-def create_recipe(request):
-    if request.method == "POST" and RecipeForm:
-        form = RecipeForm(request.POST)
-        if form.is_valid():
-            recipe = form.save()
-            return redirect("recipe_detail", pk=recipe.pk)
-    elif RecipeForm:
-        form = RecipeForm()
-    else:
-        form = None
-    context = {
-        "form": form,
-    }
-    return render(request, "recipes/new.html", context)
 
 
 def change_recipe(request, pk):
@@ -45,6 +31,11 @@ def change_recipe(request, pk):
     }
     return render(request, "recipes/edit.html", context)
 
+class RecipeCreateView(CreateView):
+    model = Recipe
+    template_name = "recipes/edit.html"
+    fields = ["name", "author", "description", "image"]
+    success_url = reverse_lazy("recipes_list")
 
 class RecipeListView(ListView):
     model = Recipe
